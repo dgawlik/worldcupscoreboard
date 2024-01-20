3f3035a24b9be1b64c8713484b558690402569c0
Dominik Gawlik <dominik.gawlik1@gmail.com>
2024-01-20 08:57:26 +0100

    api docs

--- src/main/java/com/sportsradar/ScoreBoard.java
+++ src/main/java/com/sportsradar/ScoreBoard.java
@@ -2,6 +2,20 @@ package com.sportsradar;
 
 import java.util.*;
 
+/**
+ * If race conditions occurred in between the calls of public methods the results could
+ * be undefined. Thats why all of the public methods are synchronized and atomic.
+ *
+ * The complexity:
+ * startMatch - O(logN)
+ * updateMatch - O(logN)
+ * removeMatch - O(logN)
+ * showBoard - O(N)
+ *
+ * The design decision has been made to sacrifice CRUD operations for better time
+ * of retrieval board positions in order. It makes sense as showingBoard should be
+ * most frequent operation as it is online board.
+ */
 public class ScoreBoard {
 
     private final Comparator<Match> matchComparator;
@@ -26,7 +40,15 @@ public class ScoreBoard {
         this.matchOrder = new TreeSet<>(this.matchComparator);
     }
 
-
+    /**
+     * Starts a new match and puts it in the scoreboard. Note that at all times the
+     * matches are sorted: (1) more total goals first (2) on ties most recent.
+     * The teams must be different and not occurring already on the board.
+     * Returns handle for match as string in format: HomeCountry/AwayCountry/StartSystemNanos
+     * @param homeTeam
+     * @param awayTeam
+     * @return handle
+     */
     public synchronized String startNewMatch(Team homeTeam, Team awayTeam) {
 
         Objects.requireNonNull(homeTeam, "Home team is null");
@@ -51,6 +73,14 @@ public class ScoreBoard {
         return token;
     }
 
+    /**
+     * Updates matches score, works as removing match from scoreboard and adding it with new
+     * score. Expects a handle representing a match (must be present in scoreboard). The scores
+     * must be equal or more than zero..
+     * @param handle
+     * @param homeScore
+     * @param awayScore
+     */
     public synchronized void updateMatch(String handle, int homeScore, int awayScore) {
 
         Objects.requireNonNull(handle, "Handle must be not blank");
@@ -77,6 +107,11 @@ public class ScoreBoard {
         this.matchOrder.add(match);
     }
 
+    /**
+     * Removes a match given handle. Note that the match must be present on board
+     * or else exception is thrown.
+     * @param handle
+     */
     public synchronized void removeMatch(String handle) {
         Objects.requireNonNull(handle, "Handle is null");
 
@@ -89,6 +124,10 @@ public class ScoreBoard {
         this.ongoingMatches.remove(handle);
     }
 
+    /**
+     * Returns ordered board positions. The positions are defensive copies of matches.
+     * @return
+     */
     public synchronized List<BoardPosition> showBoard() {
         return this.matchOrder
                 .stream()
